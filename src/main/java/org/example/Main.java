867e76febbef928b231dea0f955cfa7f794cd627
oybek.nurmatov <412.oybek@mail.ru>
2024-02-15 00:09:33 +0300

    12.02.2024

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       String folderPath = "C:/Users/oybek.nurmatov/Downloads";
+       File file = new File(folderPath);
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder){
+        if (folder.isFile()){
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File f : files){
+            sum += getFolderSize(f);
+        }
+        return sum;
     }
 }
